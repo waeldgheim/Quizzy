0e7983ed650f00ee756440b6958bc54c259843a4
waeldgheim <waeldgheim@hotmail.com>
2025-09-19 03:09:41 +0300

    Configured firebase on frontend

---
+++ frontend/app/firebase/config.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+// We check if apps are already initialized to avoid re-initializing on hot reloads
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Get a reference to the authentication service
+const firebaseAuth = getAuth(firebaseApp);
+
+export { firebaseApp, firebaseAuth };
